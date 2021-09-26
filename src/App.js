597ab7fdce2e90fffca8352f052d6fe6e7cf4dd0
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-26 22:03:38 +0300

    updated index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from 'react'
+import AppRouter from './router/AppRouter'
+import AuthContextProvider from "./context/AuthContext"
+import DataContextProvider from "./context/DataContext"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <AuthContextProvider>
+            <DataContextProvider>
+
+                <AppRouter />
+
+            </DataContextProvider>
+        </AuthContextProvider>
+    )
 }
 
-export default App;
+export default App
