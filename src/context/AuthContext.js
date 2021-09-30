9ab69b9567142c478db07b4dc8ce25247da05fba
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-30 12:38:38 +0300

    Created Auth Context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, createContext } from 'react'
+import { loginControl } from '../helper/firebase'
+
+
+export const AuthContext = createContext()
+
+
+const AuthContextProvider = (props) => {
+
+    const [userInfo, setUserInfo] = useState(null)
+    const [pending, setPending] = useState(true);
+
+
+    useEffect(() => {
+        loginControl(setUserInfo, setPending)
+    }, [])
+
+
+    if (pending) {
+        // console.log("pending");
+        return (
+            <div style={{
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "2em",
+            }}>
+                Loading...
+            </div>
+        )
+    }
+
+    return (
+
+        <AuthContext.Provider value={{ userInfo }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider
