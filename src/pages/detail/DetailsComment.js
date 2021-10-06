ac56e1a53ca4d6c9b07d6a00e46a90578159737b
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-10-06 22:27:56 +0300

    created details comment page

---
+++ src/pages/detail/DetailsComment.js
@@ -0,0 +1,137 @@
+import { Badge, Container, TextField } from '@mui/material'
+import {  Box } from '@mui/system'
+import React, { useContext, useState, useEffect } from 'react'
+import { Button, Typography } from '@mui/material';
+import { auth, getData, handleComment } from '../../helper/firebase';
+import { DataContext } from '../../context/DataContext';
+
+
+
+const DetailsComment = ({ comment, postId }) => {
+
+    const { setPosts} = useContext(DataContext)
+
+    const [text, setText] = useState("")
+    const [comerror, setComerror] = useState(false)
+    const [yorum, setYorum] = useState([])
+
+    useEffect(() => {
+        getData(setPosts)
+        setYorum(comment)
+    },[])
+    
+    const handleClick = () => {
+        if (text.length > 5) {
+            
+            const time = new Date().toLocaleString()
+            const writer = auth.currentUser.email
+            
+            // console.log(text)
+            // console.log(postId)
+            const newComment = {
+                comment:text,
+                time:time,
+                writer:writer,
+            }
+            handleComment(comment, newComment, postId)
+            
+            setComerror(false)
+            setText("")
+            getData(setPosts)
+
+        } else {
+            setComerror(true)
+        }
+
+    }
+
+    return (
+        <Container
+
+            sx={{
+                backgroundColor: "white",
+                width: "100%",
+                minHeight: "300px",
+            }}
+        >
+            <Box
+                variant="form"
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "stretch",
+                    justifyContent: "space-between",
+                    gridGap: "10px",
+                    padding: "10px",
+
+                }}
+            >
+
+                <TextField
+                    id="comment"
+                    name="comment"
+                    type="text"
+                    label="Comment here ..."
+                    variant="outlined"
+                    size="small"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                {comerror ? <Typography color="red" mt={-1} ml={1} >Can't be empty...</Typography> : ""}
+
+                <Button onClick={auth.currentUser?handleClick:()=>alert("giriş yapmalısın")} variant="contained">Submit</Button>
+
+            </Box>
+
+            <Box
+                sx={{
+                    width: "100%",
+                    // maxHeight: "350px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "stretch",
+                    justifyContent: "space-between",
+                    gridGap: "10px",
+                    padding: "10px",
+                    // overflowY: "auto",
+
+                }}
+            >
+                <Typography >comments...</Typography>
+                {
+                    yorum.map((item, index) => (
+                        <Box
+                            key={index}
+                            sx={{
+                                border: "solid",
+                                padding: "5px",
+                                position: "relative",
+                            }}
+                        >
+                            <Badge
+                                sx={{
+                                    bgcolor: "black",
+                                    position: "absolute",
+                                    top: "5px",
+                                    right: "5px",
+                                    color: "white",
+                                    p: "2px",
+                                    fontSize: "1.3em",
+                                    borderRadius: "2px",
+                                }}
+                            >{index +1}</Badge>
+                            <Typography color="red" fontSize="10px">{item.writer}</Typography>
+                            <Typography fontSize="10px">{item.time}</Typography>
+                            <Typography p="3px" pl={3} bgcolor="navy" color="white" >{item.comment}</Typography>
+                        </Box>
+                    ))
+                }
+
+            </Box>
+
+        </Container>
+    )
+}
+
+export default DetailsComment
